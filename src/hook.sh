cc7b62fbedbf5372ad6a9947f2bfd51f2bc7f612
Joe Groocock <me@frebib.net>
2018-04-12 17:04:41 +0100

    Add logging with colour

--- src/hook.sh
+++ src/hook.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 set -e
 
+#############################
+##      Configuration      ##
+#############################
+
+CONFIG_DIR=${CONFIG_DIR:-/config}
+
+#############################
+
 source ./util.sh
+
+checkVars -p HOOK_ repo branch status
+loginfo "Incoming notification for $HOOK_build:$HOOK_branch [$HOOK_status]"
 
 # Send a notification
 notify-telegram
 
-# TODO: Check for downstream files
+# Check for downstream files
+checkVars HOOK_repo
+if [ -f "$CONFIG_DIR/$HOOK_repo:$HOOK_branch" ]; then
+    loginfo "Triggering downstream for $HOOK_repo:$HOOK_branch"
+elif [ -f "$CONFIG_DIR/$HOOK_repo:$HOOK_branch" ]; then
+    loginfo "Triggering downstream for $HOOK_repo"
+else
+    loginfo "No downstream entries for $HOOK_repo"
+fi
+
 
 # TODO: Ensure there are no builds running
     # Queue if there is
--- src/util.sh
+++ src/util.sh
@@ -1,11 +1,53 @@
 #!/bin/sh
 
-error() { >&2 echo -e "Error: $(basename "$0"): $@"; exit 1; }
+# Colours
+BLACK='0;30'
+RED='0;31'
+GREEN='0;32'
+ORANGE='0;33'
+BLUE='0;34'
+PURPLE='0;35'
+CYAN='0;36'
+LIGHT_GRAY='0;37'
+
+DARK_GRAY='1;30'
+LIGHT_RED='1;31'
+LIGHT_GREEN='1;32'
+YELLOW='1;33'
+LIGHT_BLUE='1;34'
+LIGHT_PURPLE='1;35'
+LIGHT_CYAN='1;36'
+WHITE='1;37'
+
+ANSI_ESC='\033'
+color() { echo -e "${ANSI_ESC}[${1}m"; }
+RESET="$(color '0')"
+
+
+# Logging
+loginfo()  { log $CYAN INFO "$@"; }
+logwarn()  { log $ORANGE WARN "$@"; }
+logerror() { log $RED ERROR "$@"; }
+log() {
+    local color="$1"
+    local level="$2"
+    shift 2
+    local message="$@"
+
+    if [ -t 1 ]; then
+        echo "$(date -Isec) <$(color $YELLOW)$(basename "$0")$RESET> $(color $color)[$level]$RESET $message"
+    else
+        echo "$(date -Isec) <$(basename "$0") [$level] $message"
+    fi
+}
+
+
+error() { logerror "$@"; >&2 echo -e "Error: $(basename "$0"): $@"; exit 1; }
 
 upper() { echo $@ | tr a-z A-Z; }
 lower() { echo $@ | tr A-Z a-z; }
 
-checkVars() { 
+checkVars() {
     O=`getopt -- 'p:s:' "$@"` || exit 1
     eval set -- "$O"
     while true; do
